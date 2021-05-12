c37ca8991d6ed8bbebf18eb059fcfe325b46f2ed
AlexTheUnderdog <janikaleksa1@gmail.com>
2021-05-12 12:37:23 +0200

    Revert "Background procesi"
    This reverts commit 0746f211aec7210655cda08f5c599fc13c354f30.

--- src/AspnetRun.Application/Dnu_DataConversions/FactoryType.cs
+++ src/AspnetRun.Application/Dnu_DataConversions/FactoryType.cs
@@ -16,6 +16,9 @@ namespace AspnetRun.Application.Dnu_DataConversions
 
             switch (type)
             {
+                case Packet.DataType.Merenja:
+                    return new DataMerenja();
+
                 case Packet.DataType.RAW:
                     return new DataRAW();
 
